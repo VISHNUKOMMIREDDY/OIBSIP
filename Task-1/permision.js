61ea1f9903a699243bd5c4542b513ddc898893fb
VISHNUKOMMIREDDY <109330287+VISHNUKOMMIREDDY@users.noreply.github.com>
2023-08-02 14:27:25 +0530

    Add files via upload

---
+++ Task-1/permision.js
@@ -0,0 +1,8 @@
+var inp=prompt("Make Sure That Your Desktop Zoom Size is 75% To See My WebSite, if Done Enter 'OK' below the field.");
+if(inp==ok)
+{
+ window.alert("Welcome to MySite");
+}
+else{
+  document.write("You Have No Right To Acces MYsite");
+}
